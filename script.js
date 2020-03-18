76b009c16c614b238fc5f78a3d4ac9aacd62a0f4
Nariman Huseynov <hnariman@gmail.com>
2020-03-18 16:14:42 +0400

    added innerHTML and features

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 let button = document.querySelector('button');
 
-function getValue() {
-    let deposit = document.querySelector('#deposit').value;
-    console.log(deposit);
-    return deposit;
+function getValue(element) {
+    element = document.querySelector(`${element}`).value;
+    return element;
 }
 
-button.addEventListener('click', getValue);
+// Error checking as per technical task
+function isPositive(value) {
+    if (value === Math.abs(value)){
+        return true;
+    } else {
+        return false; 
+    }
+};
+
+function isInteger(value) {
+    if (value === Math.trunc(value)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// main body of the code:
+
+
+function calculateFinalDeposit(deposit, payment, rate, days) {
+    months = days/30;
+    console.log(`days: ${days}, months: ${months}`)
+    let result = payment * ( ((1+rate)**months - 1)/rate);
+    console.log(result);
+    // for (let i = 0; i < months; i++){
+        
+    // }
+}
+
+// calculateFinalDeposit(deposit, payment, rate, days);
+
+function printValues() {
+    let a = getValue('#deposit');
+    console.log(a);
+}
+
+document.querySelector('#result').innerHTML = "HELLO";
+
+
+button.addEventListener('click', printValues);
