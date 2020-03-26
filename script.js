cba9b05422dcde6c42bce6e4479d9476b5490997
Nariman Huseynov <hnariman@gmail.com>
2020-03-26 13:43:21 +0400

    sorted out files

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 // Getting html elements for action:
 const button = document.querySelector('button');
+const table = document.querySelector('table');
 const result = document.querySelector('#result');
+const errorMessage = document.querySelector('#error');
+
+// Getting data from user input
+const deposit = +document.querySelector('#deposit').value;
+const payment = +document.querySelector('#payment').value;
+const rate = +document.querySelector('#rate').value;
+const days = +document.querySelector('#days').value;
 
 function calculateFutureValue(deposit, payment, rate, days) {
-  // Getting data from user input
-  const deposit = +document.querySelector('#deposit').value;
-  const payment = +document.querySelector('#payment').value;
-  const rate= +document.querySelector('#rate').value;
-  const days = +document.querySelector('#days').value;
-
   // Error checking user input
-  function error(element) { alert(`${element} is not a correct value`); console.log('problem'); }
-  if (deposit != Math.abs(deposit)) { 
-    alert('deposit');
-    error(deposit); 
+  function error(element) {
+    errorMessage.innerHTML = `${element} указан неверно!`;
+    // alert(`${element} is not a correct value`);
   }
-  else if (payment != Math.abs(payment)) { error(payment); }
-  else if (rate != Math.abs(rate) || rate > 100) { error(rate); }
-  else if (days != Math.abs(days)) { error(days); }
-  else if (days != Math.trunc(days)) { error(days); }
+  if (deposit != Math.abs(deposit)) { error('deposit'); }
+  else if (payment != Math.abs(payment)) { error('payment'); }
+  else if (rate != Math.abs(rate) || rate > 100) { error('rate'); }
+  else if (days != Math.abs(days)) { error('days'); }
+  else if (days != Math.trunc(days)) { error('days'); }
   else {
+
     // variables correction
     const month = days / 30;
-    let monthlyRate = rate / 12 / 100;
+    const monthlyRate = rate / 12 / 100;
 
     // Future Value Calculation
-    let result = 0;
+    let final = deposit;
     for (increment = 0; increment < month; increment++) {
-      result += deposit * monthlyRate;
-      console.log(deposit * monthlyRate);
-      result += payment;
+      final += deposit * monthlyRate;
+      final += payment;
     }
-    result.innerHTML = ` Future value is ${result.toFixed(2)}`;
+    result.innerHTML = ` ${final.toFixed(2)}`;
+    result.style = "display:flex";
     return result;
   }
+  return NaN;
 }
-
 button.addEventListener('click', calculateFutureValue);
