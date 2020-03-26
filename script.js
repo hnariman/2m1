d1dd12c7bce8df513c0b1f77ab80a5fe3c7700f0
Nariman Huseynov <hnariman@gmail.com>
2020-03-26 18:49:39 +0400

    restructure of the code, for the TZ (())

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
-// Getting html elements for action:
+// get html elements: 
 const button = document.querySelector('button');
 const table = document.querySelector('table');
-const result = document.querySelector('#result');
 const errorMessage = document.querySelector('#error');
+const result = document.querySelector('#result');
 
-function calculateFutureValue() {
-  // Getting data from user input
+// fire alarm when error :) 
+function error(element) {
+  errorMessage.innerHTML = `${element} указан неверно!`;
+  //  alert(`${element} is not a correct value`);
+}
+
+function output(someResult, target){
+  target.innerHTML = ` ${someResult.toFixed(2)}`;
+  target.style.visibility = "visible";
+}
+
+// main calculator
+function calculateFutureValue(deposit, days, rate, payment) {
+  const month = days / 30;
+  const monthlyRate = rate / 12 / 100;
+  let final = deposit;
+
+  for (increment = 1; increment < month; increment++) {
+    final += deposit * monthlyRate;
+    final += payment;
+  }
+  output(final,result);
+  return final;
+}
+
+// main wrapper function:
+function main() {
+
+  // reset errors and indicators:
+  errorMessage.innerHTML = "";
+  result.style.visibility = "hidden";
+
+  // get data from user input
   const deposit = +document.querySelector('#deposit').value;
   const payment = +document.querySelector('#payment').value;
   const rate = +document.querySelector('#rate').value;
   const days = +document.querySelector('#days').value;
-  errorMessage.innerHTML = "";
-  result.style.visibility = "hidden";
 
-  // Error checking user input
-  function error(element) {
-    errorMessage.innerHTML = `${element} указан неверно!`;
-    // alert(`${element} is not a correct value`);
-  }
   if (deposit != Math.abs(deposit)) { error('deposit'); }
   else if (payment != Math.abs(payment)) { error('payment'); }
   else if (rate != Math.abs(rate) || rate > 100) { error('rate'); }
   else if (days != Math.abs(days)) { error('days'); }
   else if (days != Math.trunc(days)) { error('days'); }
   else {
-    // variables correction
-    const month = days / 30;
-    const monthlyRate = rate / 12 / 100;
-
-    // Future Value Calculation
-    let final = deposit;
-    for (increment = 0; increment < month; increment++) {
-      final += deposit * monthlyRate;
-      final += payment;
-    }
-    result.innerHTML = ` ${final.toFixed(2)}`;
-    result.style.visibility = "visible";
-    return result;
+    calculateFutureValue(deposit, days, rate, payment);
   }
   return NaN;
 }
